59be1c6f015d98f6bc498226edd2c958e0a19836
Pavel web2cap.ru <web2cap@gmail.com>
2022-02-09 14:13:28 +0300

    pre deploy anyware

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,8 +30,13 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "[::1]",
     "testserver",
+    "www.web2cap.pythonanywhere.com",
+    "web2cap.pythonanywhere.com",
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -47,6 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "sorl.thumbnail",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -57,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "yatube.urls"
